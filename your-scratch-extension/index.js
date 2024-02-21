0aa16e1e2caf331b6a1ccd99e7fe15f574b7acf4
Rafi Samim <66020964+rafisamim@users.noreply.github.com>
2024-02-21 16:05:29 +0430

    Update index.js

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -1,36 +1,60 @@
-class helloWorld {
-    constructor() {
+const BlockType = require('../../extension-support/block-type');
+const ArgumentType = require('../../extension-support/argument-type');
+const TargetType = require('../../extension-support/target-type');
+
+class Scratch3YourExtension {
+
+    constructor (runtime) {
+        // put any setup for your extension here
     }
-    
-    getInfo() {
+
+    /**
+     * Returns the metadata about your extension.
+     */
+    getInfo () {
         return {
-            "id": "Hello",
-            "name": "Hello",
-            color1: '#000000',
-            color2: '#fc0303',
-            "blocks": [
-                   {
-                        "opcode": "sayHelloWorld",
-                        "blockType": "reporter",
-                        "text": "Say [hello] to the [world]",
-                        "arguments": {
-                            "hello": {
-                                "type": "string",
-                                "defaultValue": "Hello"
-                            },
-                            "world": {
-                                "type": "string",
-                                "defaultValue": "World"
-                            },
+            // unique ID for your extension
+            id: 'yourScratchExtension',
+
+            // name that will be displayed in the Scratch UI
+            name: 'CircleArea',
+
+            // colours to use for your extension blocks
+            color1: '#000099',
+            color2: '#660066',
+
+            // icons to display
+            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+
+            // your Scratch blocks
+            blocks: [
+                {
+                    opcode: 'myFirstBlock',
+
+                    blockType: BlockType.REPORTER,
+
+                    text: 'Circle radius [MY_NUMBER]',
+
+                    terminal: false,
+
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    arguments: {
+                        MY_NUMBER: {
+                            defaultValue: 5,
+                            type: ArgumentType.NUMBER
                         }
-                    },
-                ],
+                    }
+                }
+            ]
         };
     }
-    sayHelloWorld () {
-        // example implementation to return a string
-        return hello;
+
+
+    myFirstBlock ({ MY_NUMBER }) {
+        return 'The circle area is: ' + (3.14 * (MY_NUMBER ** 2));
     }
 }
 
-Scratch.extensions.register(new helloWorld())
+module.exports = Scratch3YourExtension;
